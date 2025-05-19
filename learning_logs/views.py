027736e73d574ef0188d6eba071194bd95f80ef2
BrianBett125 <brianbett756@gmail.com>
2025-05-19 09:36:20 +0300

    learning logs code

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+def index(request):
+    """the homepage for learning_logs."""
+    return render(request, 'learning_logs/index.html')
+
+
 # Create your views here.
